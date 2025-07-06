7f96fe6ed73524e916db171c2f54468d64916b09
Khuram Khayam <kn.khayam@outlook.com>
2025-07-06 12:36:49 +0100

    contributions added

--- script.js
+++ script.js
@@ -254,7 +254,7 @@ class ImageSlider {
 }
 
 // Load and render contributions with accordion
-fetch('contributions.json?v=13')
+fetch('contributions.json?v=14')
   .then(res => res.json())
   .then(data => {
     const accordionContainer = document.getElementById('contributions-accordion');
